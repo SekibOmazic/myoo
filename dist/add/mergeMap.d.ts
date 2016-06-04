8d29597ba9ec683b6d173f77fb5b7c846a4ce560
Sekib Omazic <sekib.omazic@gmail.com>
2016-06-04 16:19:08 +0200

    add some docs for mergeMap

--- dist/add/mergeMap.d.ts
+++ dist/add/mergeMap.d.ts
@@ -1,4 +1,20 @@
 import { Observable } from '../core';
+/**
+ * Projects each source value to an Observable which is merged in the output Observable.
+ *
+ * Marble diagram:
+ *
+ * ```text
+ * --+--------+---------------
+ *   \        \
+ *    \       ----1----2---3--
+ *    --a--b----c----d--------
+ *             mergeMap
+ * -----a--b----c-1--d-2---3--
+ * ```
+ *
+ * @return {Observable}
+ */
 export declare function mergeMap<T, U>(projection: (val: T) => Observable<U>): Observable<U>;
 export interface MergeMapSignature<T> {
     <U>(project: (value: T) => Observable<U>): Observable<U>;
